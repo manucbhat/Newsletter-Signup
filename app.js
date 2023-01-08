4c2a96425075a83f6021e5b1b38c3da584fb0cd2
manu <manucbhat608@gmail.com>
2023-01-08 05:40:37 +0530

    added some features

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const request = require('request');
 const app = express()
 const port = 3000
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get('/', (req, res) => res.sendFile(__dirname + "/signup.html"));
+
+app.post("/",(req,res) =>{
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+
+    console.log(firstName,lastName,email);
+})
 
 app.listen(port, () => console.log(`server is running on port ${port}!`))
