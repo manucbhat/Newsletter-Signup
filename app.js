95d6a6825a75de55a852ea6ab43044915352dbe9
manu <manucbhat608@gmail.com>
2023-01-09 00:40:41 +0530

    added post rquest

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request');
+const https = require('https');
 
 const app = express()
 const port = 3000
@@ -11,11 +12,45 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get('/', (req, res) => res.sendFile(__dirname + "/signup.html"));
 
 app.post("/",(req,res) =>{
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName,lastName,email);
+    const data = {
+        members: [
+            {
+                email_address : email,
+                status:"subscribed",
+                merge_fields: {
+                    FNAME : firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us14.api.mailchimp.com/3.0/lists/0fd33bb79b";
+
+    const options = {
+        method: "POST",
+        auth : "manu:e560bbff56d402b9b68663bcdc77d173-us14"
+    }
+    const request = https.request(url,options,response =>{
+        response.on("data",data =>{
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+    
 })
 
 app.listen(port, () => console.log(`server is running on port ${port}!`))
+
+
+//list id = 0fd33bb79b
+//api key = e560bbff56d402b9b68663bcdc77d173-us14
